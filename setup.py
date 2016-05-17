feca49a59ce2e33e093c2ee9ab51157909df8169
db <danjamesbond@gmail.com>
2016-05-17 12:42:30 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,25 +1,18 @@
-import os
 from setuptools import setup
-
 
 setup(
     name='django-mysql-aesfield',
     version='0.2.1',
     description='Django Model Field that supports AES encryption in MySQL',
-    long_description=open('README.rst').read(),
     author='Andy McKay',
     author_email='andym@mozilla.com',
     license='BSD',
-    url='https://github.com/andymckay/django-mysql-aesfield',
+    url='https://github.com/c475/django-mysql-aesfield',
     include_package_data=True,
     zip_safe=False,
-    packages=['aesfield',
-              'aesfield/management',
-              'aesfield/management/commands'],
-    classifiers=[
-        'Intended Audience :: Developers',
-        'Natural Language :: English',
-        'Operating System :: OS Independent',
-        'Framework :: Django'
-        ],
-    )
+    packages=[
+        'aesfield',
+        'aesfield/management',
+        'aesfield/management/commands'
+    ]
+)
